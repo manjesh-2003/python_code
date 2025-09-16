07728c84d750c20b486626dea47f1e595c14651a
Manjesh <kumarmanjesh598@gmail.com>
2025-09-16 12:41:11 +0530

    this is the right attempt

--- lin_algebra.py
+++ lin_algebra.py
@@ -1,7 +1,7 @@
 from scipy import linalg
 import numpy as np
 
-a= np.array([[5,3,10],[3,1,0]]) #this is the coefficient matrix
-b= np.array([8,10]) #this is the constant matrix
+a= np.array([[5,3,10],[3,1,0],[1,0,1]]) #this is the coefficient matrix
+b= np.array([8,10,1]) #this is the constant matrix
 x= linalg.solve(a,b) #this is the solution matrix       
 print(x)
