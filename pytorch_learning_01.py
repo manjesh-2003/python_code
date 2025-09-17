21cee035c3e6cc356b246ee6a964f0c3113eedf0
Manjesh <kumarmanjesh598@gmail.com>
2025-09-17 08:41:31 +0530

    updated

--- pytorch_learning_01.py
+++ pytorch_learning_01.py
@@ -7,3 +7,9 @@ b= torch.tensor([[1,2],[3,4]], dtype=torch.float32)
 #Tensor operations
 print(a+2) #element wise addition
 print(b.T) #transpose of a tensor
+
+#for gradient calculation
+x= torch.tensor(2.0, requires_grad=True)
+y= x**2 + 3*x + 2 #function
+y.backward() #compute dy/dx
+print(x.grad) #print 7 (derivative at x=2)
